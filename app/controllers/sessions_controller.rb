9a943107ba2be9cfb99dc897911007c4404f200d
hansenjl <jenn.leigh.hansen@gmail.com>
2020-07-13 21:12:26 -0700

    add login action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,18 @@ class SessionsController < ApplicationController
     end
 
     def create
+        @user = User.find_by(username: params[:user][:username])
+        if @user.authenticate(params[:user][:password])
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else
+            flash[:notice] = 'Log in failed. Please try again.'
+            render :new
+        end
     end
 
     def destroy
+        session.delete :user_id
+        redirect_to root_path
     end
 end
