6d47aec3bdf3adc55fa36e241a14edaaf320f1fe
hansenjl <jenn.leigh.hansen@gmail.com>
2020-07-13 21:01:13 -0700

    set up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  resources :purchases
-  resources :items
+  root: 'items#index'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/signup' => 'users#new'
+  post '/signup' => 'users#create'
+  delete '/logout' => 'sessions#destroy'
+
+  resources :purchases, only: [:show, :new, :create]
+  resources :items, only: [:index, :show]
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
