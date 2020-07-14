f7d6ed2d1ea12b36264374112b6aed0c0d85e20c
hansenjl <jenn.leigh.hansen@gmail.com>
2020-07-13 21:42:14 -0700

    fix type - missing question mark

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
         @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
     end
 
-    def logged_in
-    !!current_user
+    def logged_in?
+        !!current_user
     end
 end
