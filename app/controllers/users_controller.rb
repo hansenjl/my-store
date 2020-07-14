fd4330adc3f8f33b6926f9f1a275e7f477dc3c94
hansenjl <jenn.leigh.hansen@gmail.com>
2020-07-13 21:55:09 -0700

    add balance to signup form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,6 +22,6 @@ class UsersController < ApplicationController
     private
 
     def user_params
-        params.require(:user).permit(:username, :email, :password)
+        params.require(:user).permit(:username, :email, :balance, :password)
     end
 end
